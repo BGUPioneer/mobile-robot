bab3d0623cf8c34975fe4ffd57b8db9a804db3bf
drorkatz11 <drorkatz11@gmail.com>
2015-12-27 15:28:11 +0200

    left

--- occlusions/src/depth_occlusions.cpp
+++ occlusions/src/depth_occlusions.cpp
@@ -22,9 +22,14 @@
 
 #define PI 3.14159265
 
+cv::Scalar red(0,255,0);
 
 ros::Publisher cmd_vel_pub;
 geometry_msgs::Twist cmd_vel;
+
+double AgeThreshold=0;
+double ConfidenceTheshold=1.1;
+double HeightTheshold=1.4;
 
 namespace enc = sensor_msgs::image_encodings;
 
@@ -37,26 +42,38 @@ class ImageConverter
     image_transport::Subscriber image_sub;
     image_transport::Publisher image_pub;
 
-    opt_msgs::DetectionArray opt_[];
-  //  opt_msgs::Subscriber person_sub;
-    ros::Subscriber person_sub = n.subscribe("/detector/detections", 10, &ImageConverter::boxCallback, this);
+  //  opt_msgs::DetectionArray opt_[];
+ //   opt_msgs::TrackArray opt_[];
+  //  ros::Subscriber person_sub = n.subscribe("/detector/detections", 10, &ImageConverter::boxCallback, this);
+  //  ros::Subscriber sub = n.subscribe("/tracker/tracks", 10, &ImageConverter::personCallback, this);
+    ros::Subscriber person_sub = n.subscribe("/tracker/tracks", 10, &ImageConverter::boxCallback, this);
 
-  //  Subscriber person_sub = n.subscribe("/detector/detections", 10, &ImageConverter::boxCallback, this);
+
     double xmin;
     double ymin;
     double xmax;
     double ymax;
     double personCentroid;
-    double xcentroid;
-    double ycentroid;
+ //   double xcentroid;
+ //   double ycentroid;
+    double distance;
+    double confidence;
+    double age;
+    double height;
+    double xc;
+    double yc;
+    double depth;
+    double depthTheshold=500;
+    bool validTrack;
+
 
 public:
     ImageConverter()
       : it_(n)
     {
       // Subscrive to input video feed and publish output video feed
-    //  image_sub = it_.subscribe("/kinect2_head/depth_rect/image", 10, &ImageConverter::imageCallback, this);
-      image_sub = it_.subscribe("/kinect2_head/ir_rect_eq/image", 10, &ImageConverter::imageCallback, this);
+      image_sub = it_.subscribe("/kinect2_head/depth_rect/image", 10, &ImageConverter::imageCallback, this);
+    //  image_sub = it_.subscribe("/kinect2_head/ir_rect_eq/image", 10, &ImageConverter::imageCallback, this);
 
         //kinect2_head/ir_rect_eq/image
         //kinect2_head/depth_rect/image
@@ -71,19 +88,27 @@ public:
     }
 
 
-void boxCallback(const opt_msgs::DetectionArray::ConstPtr& msg){
+void boxCallback(const opt_msgs::TrackArray::ConstPtr& msg){
 
+    bool validTrack=false;
 
-
-    int nbOfTracks=msg->detections.size();
+    int nbOfTracks=msg->tracks.size();
      if (nbOfTracks>0) {
-    xmin=msg->detections[0].box_2D.x;
-    ymin=msg->detections[0].box_2D.y;
-    xmax=xmin+msg->detections[0].box_2D.width;
-    ymax=ymin+msg->detections[0].box_2D.height;
-    personCentroid=msg->detections[0].centroid.z;
-    xcentroid=msg->detections[0].centroid.x;
-    ycentroid=msg->detections[0].centroid.y;
+         for(int i=0;i<nbOfTracks && !validTrack;i++){
+             //oldest track which is older than the age threshold and above the confidence threshold
+             if ((msg->tracks[i].age>AgeThreshold) && (msg->tracks[i].confidence>ConfidenceTheshold) && (msg->tracks[i].height>HeightTheshold)){
+    xmin=msg->tracks[i].box_2D.x;
+    ymin=msg->tracks[i].box_2D.y;
+    xmax=xmin+msg->tracks[i].box_2D.width;
+    ymax=ymin+msg->tracks[i].box_2D.height;
+    distance=msg->tracks[0].distance;
+    confidence=msg->tracks[i].confidence;
+    height=msg->tracks[i].height;
+    age=msg->tracks[i].age;
+
+  //  xc=(xmin+xmax)/2;
+ //   yc=ymin+(ymax-ymin)/3; //the 1/3 upper body
+
 
 
 //    ROS_INFO("Depth: %d", depth);
@@ -91,11 +116,17 @@ void boxCallback(const opt_msgs::DetectionArray::ConstPtr& msg){
     ROS_INFO("ymin: %f", ymin);
     ROS_INFO("xmax: %f", xmax);
     ROS_INFO("ymax: %f", ymax);
-    ROS_INFO("personCentroid: %f", personCentroid);
-    ROS_INFO("xcentroid: %f", xcentroid);
-    ROS_INFO("ycentroid: %f", ycentroid);
+    ROS_INFO("Confidence: %f", confidence);
+    ROS_INFO("Height: %f", height);
+    ROS_INFO("age: %f", age);
+    ROS_INFO("distance: %f", distance);
+ //   ROS_INFO("personCentroid: %f", personCentroid);
+ //   ROS_INFO("xcentroid: %f", xcentroid);
+ //   ROS_INFO("ycentroid: %f", ycentroid);
+validTrack=true;
 
-
+            }
+        }
     }
 }
 
@@ -113,15 +144,32 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
       return;
     }
 
-    double xc=(xmin+xmax)/2;
-    double yc=ymin+(ymax-ymin)/3; //the 1/3 upper body
 
-    double depth = cv_ptr->image.at<short int>(cv::Point(xc,yc));//milimeters for topic kinect2_head/depth_rect/image. and -XXXXX for topic kinect2_head/ir_rect_eq/image  -the amount of infrared light reflected back to the camera.
+    xc=(xmin+xmax)/2;
+    yc=ymin+(ymax-ymin)/3; //the 1/3 upper body
+    depth = cv_ptr->image.at<short int>(cv::Point(xc,yc));//milimeters for topic kinect2_head/depth_rect/image. and -XXXXX for topic kinect2_head/ir_rect_eq/image  -the amount of infrared light reflected back to the camera.
     ROS_INFO("depth: %f", depth);
 
-  //  cv::circle(cv_ptr->image, cv::Point(xc, yc), 50, cv::Scalar(255,255,255));
-    cv::rectangle(cv_ptr->image, cv::Point(xmin, ymin),	cv::Point(xmax, ymax), cv::Scalar(0,0,0), CV_FILLED, 8);
-              cv::putText(cv_ptr->image, "test", cv::Point(xc, yc), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255,255,255), 1.7, CV_AA);
+    cv::rectangle(cv_ptr->image, cv::Point(xmin, ymin),	cv::Point(xmax, ymax), red, CV_FILLED, 8);
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if (validTrack){
+    int left=0;
+    bool leftOcclusions=false;
+        for (int i=xmin;i<xc-5;i++){
+        for (int j=ymin;j<ymax;j++){
+         //   if (cv_ptr->image.at<short int>(cv::Point(i,j))<(depth-depthTheshold)){left++;}
+               if (cv_ptr->image.at<short int>(cv::Point(i,j))<(depth-depthTheshold)){left++;}
+
+     }
+    }
+        if (left>5000)
+            {leftOcclusions=true;
+            ROS_INFO("leftOcclusions: true %f", left);}
+        else {ROS_INFO("leftOcclusions: false");}
+}
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Update GUI Window
     cv::imshow(OPENCV_WINDOW, cv_ptr->image);
@@ -147,97 +195,4 @@ int main(int argc, char **argv){
 	 ros::spin();
 	  return 0;
 }
-
-////////////////////////////////////
-/*
-
-namespace enc = sensor_msgs::image_encodings;
-//double xmin;
-//double ymin;
-//double xmax;
-//double ymax;
-//double personCentroid;
-//double xcentroid;
-//double ycentroid;
-
-static const std::string OPENCV_WINDOW = "Image window";
-
-class Listener
-{
-public:
-  void boxCallback(const opt_msgs::DetectionArray::ConstPtr& msg);
-  void imageCallback(const sensor_msgs::ImageConstPtr& msg);
-};
-
-//cv::Mat roi( depth_image, cv::Rect( left, top, width, height ) );
-
-
-void Listener::boxCallback(const opt_msgs::DetectionArray::ConstPtr& msg){
-    int nbOfTracks=msg->detections.size();
-     if (nbOfTracks>0) {
-    double xmin=msg->detections[0].box_2D.x;
-    double ymin=msg->detections[0].box_2D.y;
-    double xmax=xmin+msg->detections[0].box_2D.width;
-    double ymax=ymin+msg->detections[0].box_2D.height;
-    double personCentroid=msg->detections[0].centroid.z;
-    double xcentroid=msg->detections[0].centroid.x;
-    double ycentroid=msg->detections[0].centroid.y;
-
-
-//    ROS_INFO("Depth: %d", depth);
-    ROS_INFO("xmin: %f", xmin);
-    ROS_INFO("ymin: %f", ymin);
-    ROS_INFO("xmax: %f", xmax);
-    ROS_INFO("ymax: %f", ymax);
-    ROS_INFO("personCentroid: %f", personCentroid);
-    }
-}
-
-
-void Listener::imageCallback(const sensor_msgs::ImageConstPtr& msg)
-{
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_16UC1);//now cv_ptr is the matrix
-
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
-
-    double depth = cv_ptr->image.at<short int>(cv::Point(300,700));//you can change 240,320 to your interested pixel
-    ROS_INFO("depth: %f", depth);
-
-
-}
-
-int main(int argc, char **argv){
-
-
-    ros::init(argc, argv, "listener_class");
-    ros::NodeHandle n;
-    Listener Listener;
-
-     ros::Subscriber sub = n.subscribe("/kinect2_head/depth_rect/image", 10, &Listener::imageCallback, &Listener);
-     ros::Subscriber sub1 = n.subscribe("/detector/detections", 10, &Listener::boxCallback, &Listener);
-
-     //kinect2_head/ir_rect_eq/image
-//kinect2_head/depth_rect/image
-     ros::spin();
-      return 0;
-}
-*/
-
-
-
-
-
-
-
-
-
-
 
